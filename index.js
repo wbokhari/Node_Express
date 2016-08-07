81f91e2301253dd719236be3bce57ba394ef5175
Wasif Bokhari <wasif.bokhari@intel.com>
2016-08-06 17:03:44 -0700

    Setup Project

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by wbokhari on 8/6/2016.
+ */
+
+var express = require('express');
+
+var app = express();
+
+app.get('/notes', function(req, res) {
+    res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
+});
+
+app.listen(3000);
