a25eb6b59f8363d53006371ca6989fb786d91fc4
Danish Jalil <danishjalil@Danishs-MacBook-Pro.local>
2016-08-08 01:18:32 +0500

    Minor change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express();
 
 app.get('/notes', function(req, res) {
-    res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
+    res.json({notes: "This is your notebook. Okay Edit this to start saving your notes!"})
 });
 
 app.listen(3000);
